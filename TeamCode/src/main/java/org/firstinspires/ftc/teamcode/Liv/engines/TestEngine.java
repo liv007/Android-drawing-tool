187e22e0d7b3007b67592430122b54c9f3987f6c
Liv007 <Olivia55045@gmail.com>
2018-03-31 11:36:11 -0500

    made a state

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Liv/engines/TestEngine.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Liv.engines;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.engine.Engine;
+import org.firstinspires.ftc.teamcode.Liv.states.motors.dcDrive;
+
+/**
+ * Created by Liv on 3/31/2018.
+ */
+@Autonomous(name="TestEngine")
+public class TestEngine extends Engine {
+    @Override
+    public void setProcesses() {
+        addState(new dcDrive(this));
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Liv/states/motors/dcDrive.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Liv.states.motors;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.engine.Engine;
+import org.engine.State;
+
+/**
+ * Created by Liv on 3/31/2018.
+ */
+
+public class dcDrive extends State {
+    private DcMotor motor ;
+    //constructor
+    public dcDrive (Engine localEngine){
+        this.engine = localEngine;
+    }
+    @Override
+    public void init () {
+        motor = this.engine.hardwareMap.dcMotor.get("motor");
+
+    }
+    @Override
+    public void exec() {
+        motor.setPower(1.0);
+    }
+}
